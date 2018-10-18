d18a1dcd7430dbdb6164cbbf323cf23346fc481b
= <suryatriyana@gmail.com>
2018-10-18 21:41:25 +0700

    update db support Cloud SQL by Google Cloud Platform

--- db.go
+++ db.go
@@ -2,7 +2,6 @@ package framework
 
 import (
 	"database/sql"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"os"
 )
@@ -11,11 +10,7 @@ func MysqlConnect() (*sql.DB, error) {
 	var db *sql.DB
 	var err error
 	if os.Getenv("mysqlGoogle") == "1" {
-		connect := os.Getenv("CONNECTION")
-		user := os.Getenv("USER")
-		pass := os.Getenv("PASS")
-		dbName := os.Getenv("DB")
-		db, err = sql.Open("mysql", fmt.Sprintf("%s:%s@cloudsql(%s)/%s",connect,user,pass,dbName))
+		db, err = sql.Open("mysql", os.Getenv("MYSQL_CONNECTION"))
 	}else{
 		db, err = sql.Open("mysql", os.Getenv("mysqlUser")+":"+
 			os.Getenv("mysqlPwd")+"@"+os.Getenv("mysqlHost")+"/"+os.Getenv("mysqlDb"))
