cef87c4715dcf789ddab9e6ec6ee3c5917c0f5b8
= <suryatriyana@gmail.com>
2018-10-23 17:16:08 +0700

    resize change package

--- image.go
+++ image.go
@@ -1,112 +1,100 @@
 package framework
 
 import (
-	"errors"
-	"github.com/nfnt/resize"
+	"cloud.google.com/go/storage"
+	"context"
+	"fmt"
+	"github.com/disintegration/imaging"
+	"github.com/satori/go.uuid"
 	"image"
-	"image/gif"
-	"image/jpeg"
-	"image/png"
+	"log"
 	"mime/multipart"
-	"os"
-	"path/filepath"
-	"strings"
+	"path"
+	"time"
 )
 
 type Image struct {
 	img image.Image
 	ext string
 	file *multipart.FileHeader
+	Encrypt bool
+	Width int
+	Height int
 }
 
-func(img *Image) Set(path string) error {
-	ext := strings.ToLower(filepath.Ext(path))
-	img.ext = ext
-	file, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-	switch ext {
-	case ".png" :
-		img.img, err = png.Decode(file)
-	case ".gif" :
-		img.img, err = gif.Decode(file)
-	default:
-		img.img, err = jpeg.Decode(file)
-	}
-	if err != nil {
-		return err
-	}
-	file.Close()
-	return  nil
-}
-
-func(img *Image) Resize(newSize uint,newPath string) error {
-	if img.img == nil{
-		return errors.New("Set Path Before")
-	}
-	image := resize.Resize(newSize, 0, img.img, resize.Lanczos3)
-	out, err := os.Create(newPath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-	switch img.ext {
-	case ".png" :
-		png.Encode(out, image)
-	case ".gif" :
-		gif.Encode(out, image, nil)
-	default:
-		jpeg.Encode(out, image, nil)
-	}
-	return  nil
+type Filename struct {
+	Filename string
+	Dir string
+	Fullpath string
 }
 
 
-
-func(img *Image) SetFile(file *multipart.FileHeader) error {
-	ext := strings.ToLower(filepath.Ext(file.Filename))
-	img.ext = ext
+func(img *Image) Set(file *multipart.FileHeader) error {
+	img.Encrypt = false
+	f,err := file.Open()
+	if err!= nil {
+		return err
+	}
+	defer f.Close()
+	src, err := imaging.Decode(f)
+	if err!= nil {
+		return err
+	}
+	img.img = src
 	img.file = file
-
-	f, err := file.Open()
-	if err != nil {
-		return err
-	}
-	switch ext {
-	case ".png" :
-		img.img, err = png.Decode(f)
-	case ".gif" :
-		img.img, err = gif.Decode(f)
-	default:
-		img.img, err = jpeg.Decode(f)
-	}
-	if err != nil {
-		return err
-	}
-	f.Close()
-	return  nil
+	return nil
 }
 
-func(img *Image) GoogleUpload(newSize uint,bucket,filename string) (string,error) {
-	if img.img == nil{
-		return "",errors.New("Set Path Before")
+func(img *Image) ResizeSave(location string)  (Filename,error){
+	var f Filename
+	img.img = imaging.Resize(img.img,img.Width,img.Height, imaging.Lanczos)
+	filename := img.file.Filename
+	if img.Encrypt {
+		filename = string(time.Now().Unix())+uuid.Must(uuid.NewV4()).String() + path.Ext(filename)
 	}
-	image := resize.Resize(newSize, 0, img.img, resize.Lanczos3)
-	tmp := "tmp"+img.ext
-	out,err := os.Create(tmp)
-
-	switch img.ext {
-	case ".png" :
-		png.Encode(out, image)
-	case ".gif" :
-		gif.Encode(out, image, nil)
-	default:
-		jpeg.Encode(out, image, nil)
-	}
-	url,err := StorageUpload(tmp,bucket,filename)
+	f.Dir = location
+	f.Filename = filename
+	f.Fullpath = location+filename
+	err := imaging.Save(img.img, f.Fullpath)
 	if err != nil {
-		return "",err
+		return Filename{},err
 	}
-	return  url,nil
+	return f,nil
+}
+
+func(img *Image) ResizeUpload(bucket string)  (Filename,error){
+	var f Filename
+	img.img = imaging.Resize(img.img,img.Width,img.Height, imaging.Lanczos)
+	filename := img.file.Filename
+	filename = string(time.Now().Unix())+uuid.Must(uuid.NewV4()).String() + ".png"
+	f.Dir = "https://storage.googleapis.com/"
+	f.Filename = filename
+	f.Fullpath = f.Dir+filename
+
+
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	wc := client.Bucket(bucket).Object(filename).NewWriter(ctx)
+	//wc.ACL = []storage.ACLRule{{Entity: storage.AllUsers, Role: storage.RoleReader}}
+
+	//wc.ContentType = img.file.Header.Get("Content-Type")
+
+	// Entries are immutable, be aggressive about caching (1 day).
+	wc.CacheControl = "public, max-age=86400"
+
+	err1 := imaging.Encode(wc, img.img,imaging.PNG )
+	if err1 != nil {
+		fmt.Println("to bucket ", err1)
+	}
+
+	if err := wc.Close(); err != nil {
+		fmt.Println("error wc close ", err)
+		return Filename{},err
+	}
+
+	return f,nil
 }
