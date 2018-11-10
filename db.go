59dafb5e6f2befbc8a8b29ab1bd82fb819819e36
Triyana Suryapraja Sukmana <suryatriyana@gmail.com>
2018-11-10 10:22:55 +0700

    update framework for support app enginne

--- db.go
+++ db.go
@@ -9,7 +9,7 @@ import (
 func MysqlConnect() (*sql.DB, error) {
 	var db *sql.DB
 	var err error
-	if os.Getenv("mysqlGoogle") == "1" {
+	if os.Getenv("MYSQL_CONNECTION") != "" {
 		db, err = sql.Open("mysql", os.Getenv("MYSQL_CONNECTION"))
 	}else{
 		db, err = sql.Open("mysql", os.Getenv("mysqlUser")+":"+
