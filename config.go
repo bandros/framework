d55da4dceec8114d960e7e5356775e499faa330a
Triyana Suryapraja Sukmana <suryatriyana@gmail.com>
2019-02-27 11:51:35 +0700

    fix error with config.yaml not found

--- config.go
+++ config.go
@@ -1,13 +1,9 @@
 package framework
 
 import (
-	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/cookie"
-	"github.com/gin-gonic/autotls"
 	"github.com/gin-gonic/gin"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
-	"net/http"
 	"os"
 )
 
@@ -25,35 +21,10 @@ func (r *Init) Get() {
 		gin.SetMode(gin.ReleaseMode)
 	}
 	r.Begin = gin.Default()
-
-	store := cookie.NewStore([]byte(Config("sessionKey")))
-	r.Begin.Use(sessions.Sessions(Config("sessionName"), store))
-	//config.Router(r)
-	//r.Run(":"+os.Getenv("portHost"))
-
 }
 
 func (r *Init) Run() {
 	r.Begin.Run(":" + os.Getenv("portHost"))
-}
-
-func redirect(w http.ResponseWriter, req *http.Request) {
-	// remove/add not default ports from req.Host
-	target := "https://" + req.Host + req.URL.Path
-	if len(req.URL.RawQuery) > 0 {
-		target += "?" + req.URL.RawQuery
-	}
-	http.Redirect(w, req, target, http.StatusPermanentRedirect)
-}
-
-func (r *Init) RunTls(domain ...string) error {
-	go http.ListenAndServe(":80", http.HandlerFunc(redirect))
-	return autotls.Run(r.Begin, domain...)
-}
-
-func (r *Init) RunCert(cert, key string) {
-	go http.ListenAndServe(":80", http.HandlerFunc(redirect))
-	http.ListenAndServeTLS(":443", cert, key, r.Begin)
 }
 
 func Config(key string) string {
@@ -64,10 +35,16 @@ func ReloadConfig() {
 	var cfg ConfigNew
 	file, err := ioutil.ReadFile("config.yml")
 	if err != nil {
-		file, _ = ioutil.ReadFile("config.yaml")
+		file,err = ioutil.ReadFile("config.yaml")
+		if err != nil {
+			panic(err.Error())
+		}
 	}
 	yaml.Unmarshal(file, &cfg)
-	for i, v := range cfg.Config[0] {
-		os.Setenv(i, v)
+	if len(cfg.Config) >= 1 {
+		for i, v := range cfg.Config[0] {
+			os.Setenv(i, v)
+		}
 	}
+
 }
