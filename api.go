069f683575c679e56935810be4242e34a1812291
tss <suryatriyana@gmail.com>
2020-05-01 11:51:27 +0700

    test

--- api.go
+++ api.go
@@ -82,7 +82,7 @@ func (api *Api) Do(method string) error {
 		}
 		fmt.Println(param.Encode())
 		if api.ContentType == "application/x-www-form-urlencoded" {
-			payload := strings.NewReader("=mocha.fiqri%40gmail.com&fields%5Balamat%5D=-&fields%5Bid_member%5D=120033000020&fields%5Bno_hp%5D=6281313120403&fields%5Btanggal_daftar%5D=2020-05-01+11%3A47%3A34&full_name=Mochamad+Fiqri+Al+Muhafizd&list=51154&tags=pendaftaran")
+			payload := strings.NewReader("email=mocha.fiqri%40gmail.com&fields%5Balamat%5D=-&fields%5Bid_member%5D=120033000020&fields%5Bno_hp%5D=6281313120403&fields%5Btanggal_daftar%5D=2020-05-01+11%3A47%3A34&full_name=Mochamad+Fiqri+Al+Muhafizd&list=51154&tags=pendaftaran")
 			fmt.Println("ok5")
 			req, err = http.NewRequest(method, api.Url, payload)
 		} else {
